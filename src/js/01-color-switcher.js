ad03e3e1464d609854805124bc939f70cb8bddfd
Iryusha <Sherbinina_Irina@ukr.net>
2022-02-04 11:02:25 +0200

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startItem = document.querySelector('[data-start]');
+const stopItem = document.querySelector('[data-stop]');
+const bodyItem = document.querySelector('body');
 
+startItem.addEventListener('click', onStartRundomColor);
+stopItem.addEventListener('click', onStopRundomColor);
+
+let timerId = null;
+let isActive = false;
+
+function onStartRundomColor() {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  addsRundomColor();
+
+  timerId = setInterval(addsRundomColor, 1000);
+}
+
+function onStopRundomColor() {
+  clearTimeout(timerId);
+  isActive = false;
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function addsRundomColor() {
+  bodyItem.style.backgroundColor = getRandomHexColor();
+}
